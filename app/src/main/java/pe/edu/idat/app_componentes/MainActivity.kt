fad0f4d4f2fdef95d574ebe06f8672ddabec0a28
idatsalon54-log <idatsalon54@gmail.com>
2026-02-11 08:15:32 -0500

    definicion de variables en activity

--- app/src/main/java/pe/edu/idat/app_componentes/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/app_componentes/MainActivity.kt
@@ -5,8 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import pe.edu.idat.app_componentes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val listPreferencias = ArrayList<String>()
+    private val listUsuario = ArrayList<String>()
+    private var estadoCivil = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
