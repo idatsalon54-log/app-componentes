fb2dd00ad625d0e5e6149f8b11821f1bb9d42d5e
idatsalon54-log <idatsalon54@gmail.com>
2026-02-11 07:56:02 -0500

    diseño terminado, corrección de fix de id al constraint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "pe.edu.idat.app_componentes"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "pe.edu.idat.app_componentes"
